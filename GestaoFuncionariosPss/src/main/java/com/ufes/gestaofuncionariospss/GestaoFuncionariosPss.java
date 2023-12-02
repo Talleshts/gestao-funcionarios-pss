1528bea96f719d916be8fe5bdfb37e647fc705ae
talleshts <talles.santos@itixti.com.br>
2023-12-02 17:50:08 -0300

    inicio do projeto

---
+++ GestaoFuncionariosPss/src/main/java/com/ufes/gestaofuncionariospss/GestaoFuncionariosPss.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.ufes.gestaofuncionariospss;
+
+/**
+ *
+ * @author tallesh
+ */
+public class GestaoFuncionariosPss {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
